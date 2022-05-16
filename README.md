4ed5ca826ef1143c130e8ff4ae13b63712e5d0a0
Nikita Akimov <korchiy@yandex.ru>
2022-05-16 16:02:37 +0300

    update

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# blender_psycopg2_module_install
-Psycopg2 module install to Blender
+# Psycopg2 module install to Blender
+
+This script install the Psycopg2 mudule to Blender.
+
+This module is required to connect to PostgreSQL databases from Blender.
+
+Usage
+-
+- Run blender with the Administrative (root) priveleges.
+- Execute this script from the Blender Text Editor.
---
+++ psycopg2_install.py
@@ -0,0 +1,13 @@
+import subprocess
+import sys
+import os
+ 
+python_exe = os.path.join(sys.prefix, 'bin', 'python.exe')
+target = os.path.join(sys.prefix, 'lib', 'site-packages')
+ 
+subprocess.call([python_exe, '-m', 'ensurepip'])
+subprocess.call([python_exe, '-m', 'pip', 'install', '--upgrade', 'pip'])
+ 
+subprocess.call([python_exe, '-m', 'pip', 'install', '--upgrade', 'psycopg2', '-t', target])
+ 
+print('Psycopg2 installed')
